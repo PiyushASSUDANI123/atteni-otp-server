ce98d896f76f6816f69bf368c4d406316817ce42
Piyush Assudani <piyushassudani96@gmail.com>
2026-06-13 15:07:37 +0530

    Update puppeteer config with executablePath

--- index.js
+++ index.js
@@ -28,15 +28,8 @@ const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
-        args: [
-            '--no-sandbox', 
-            '--disable-setuid-sandbox', 
-            '--disable-dev-shm-usage',
-            '--disable-accelerated-2d-canvas',
-            '--no-first-run',
-            '--no-zygote',
-            '--disable-gpu'
-        ]
+        executablePath: '/usr/bin/chromium-browser', // Yahi path use karo jo 'which' command ne diya
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
     }
 });
 
